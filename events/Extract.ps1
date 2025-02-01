9151be283af41cab6874ae564e2ab3e2a63c52a7
arousseau00 <77174802+arousseau00@users.noreply.github.com>
2025-01-31 23:17:01 -0500

    Enhance icon extraction and preview functionality in Extract.ps1 and MainWindow.ps1; update file dialog filters and enable Save As button.

--- events/Extract.ps1
+++ events/Extract.ps1
@@ -4,32 +4,53 @@
 #>
 
 $Extract.Add_Loaded({
-        # Set the default icon size.
-        $Extract_IconSizeSlider.Value = 9
+    # Set the default icon size.
+    $Extract_IconSizeSlider.Value = 9
 })
 
 $Extract_IconSizeSlider.Add_ValueChanged({
-        # Update the icon label.
-        $size = [System.Math]::Pow(2, $Extract_IconSizeSlider.Value)
-        $Extract_IconSizeValue.Content = '{0}x{0}' -f $size
+    # Update the icon label.
+    $size = [System.Math]::Pow(2, $Extract_IconSizeSlider.Value)
+    $Extract_IconSizeValue.Content = '{0}x{0}' -f $size
 
-        # Update the icon preview.
-        $Extract_IconPreviewImage.Width = $size
-        $Extract_IconPreviewImage.Height = $size
+    # Update the icon preview.
+    $Extract_IconPreviewImage.Width = $size
+    $Extract_IconPreviewImage.Height = $size
+    if ($Extract_IconPreviewImage.Tag) {
+        $paramSetIconPreview = @{
+            Control  = $Extract_IconPreviewImage
+            FilePath = $Extract_IconPreviewImage.Tag
+            Size     = $size
+        }
+        Set-IconPreview @paramSetIconPreview
+    }
 })
 
 $Extract_BrowseButton.Add_Click({
-        # Open the file dialog.
-        $dialog = [Microsoft.Win32.OpenFileDialog]::new()
-        $dialog.Filter = 'All Files (*.*)|*.*'
-        $dialog.ShowDialog() | Out-Null
-        $Extract_IconPreviewImage.Source = $dialog.FileName
+    # Open the file dialog.
+    $dialog = [Microsoft.Win32.OpenFileDialog]::new()
+    $dialog.Filter = 'Executables (*.exe)|*.exe'
+    if ($dialog.ShowDialog()) {
+        $Extract_IconPreviewImage.Tag = $dialog.FileName
+        $paramSetIconPreview = @{
+            Control  = $Extract_IconPreviewImage
+            FilePath = $dialog.FileName
+            Size     = $Extract_IconPreviewImage.Width
+        }
+        Set-IconPreview @paramSetIconPreview
+    }
 })
 
 $Extract_SaveAsButton.Add_Click({
-        # Open the file dialog.
-        $dialog = [Microsoft.Win32.SaveFileDialog]::new()
-        $dialog.Filter = 'All Files (*.*)|*.*'
-        $dialog.ShowDialog() | Out-Null
-        $dialog.FileName
+    # Open the file dialog.
+    $dialog = [Microsoft.Win32.SaveFileDialog]::new()
+    $dialog.Filter = 'PNG Files (*.png)|*.png|JPEG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|BMP Files (*.bmp)|*.bmp'
+    if ($dialog.ShowDialog()) {
+        Save-Image -ImageSource $Extract_IconPreviewImage.Source -FilePath $dialog.FileName
+    }
+})
+
+$Extract_IconPreviewImage.Add_DpiChanged({
+    # When an image is loaded, enable the save as button.
+    $Extract_SaveAsButton.IsEnabled = $true
 })
