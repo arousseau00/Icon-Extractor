b64c0f442e4921458f2391721f2cc028cf802fe7
arousseau00 <77174802+arousseau00@users.noreply.github.com>
2025-02-11 23:43:24 -0500

    Add default settings management and enhance settings UI in Startup.ps1 and Settings.xaml

--- Startup.ps1
+++ Startup.ps1
@@ -1,18 +1,23 @@
-# Load required assembly.
+# MARK: Assembly
+# Load required assembly and files.
 @('PresentationFramework', 'PresentationCore', 'System.Xml', 'System.Xaml') | ForEach-Object {
     [System.Reflection.Assembly]::LoadWithPartialName($_) | Out-Null
 }
-
-# Load the Functions.ps1 file.
 . '.\resources\Functions.ps1'
 
 # Create the application.
 $Global:Application = [System.Windows.Application]::new()
 
-# Set the UI locale and styles.
-Set-AppUILocale
+# MARK: Settings
+# Ensure settings are set and apply them.
+$Global:DefaultSettings = @{
+    Locale = 'en'
+    IconSize = 256
+}
+Set-DefaultSettings -Path "$env:APPDATA\IconExtractor\settings.json" -DefaultSettings $DefaultSettings
 Add-WPFResource -Target $Application -Path '.\resources\Styles.xaml'
 
+# MARK: Interfaces
 # Load interfaces and events.
 Get-ChildItem -Path '.\pages\*.xaml' | New-WPFControl
 Get-ChildItem -Path '.\events\*.ps1' | ForEach-Object { . $_.FullName }
